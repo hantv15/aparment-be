ce6a1f463de3e99fecadc4a0d7035f64f4553fc0
tuanna3 <tuanna3@vnext.com.vn>
2022-01-20 14:05:44 +0700

    fix: fix class not found

--- config/app.php
+++ config/app.php
@@ -176,7 +176,6 @@ return [
         App\Providers\RouteServiceProvider::class,
         Laravel\Passport\PassportServiceProvider::class,
         L5Swagger\L5SwaggerServiceProvider::class,
-        Mtrajano\LaravelSwagger\SwaggerServiceProvider::class
 
     ],
 
