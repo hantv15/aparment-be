b0f05b6bcf0804086525057990d16d5670350ad7
Tài Duy <duyntph12791@fpt.edu.vn>
2022-02-16 15:49:17 +0700

    filter department by tower, phone number and name

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\DepartmentResource;
 use App\Models\Apartment;
+use App\Models\Bill;
 use App\Models\Department;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -12,12 +13,24 @@ class DepartmentController extends Controller
 {
     public function getDepartment(Request $request): JsonResponse
     {
-
+        
         $departments = Department::all();
-        if ($request->filled('floor')) {
-            $departments = Department::where('floor', $request->floor)->get();
+        if ($request->filled('department_id') || $request->filled('keyword')) {
+            $departments = Department::join('users', 'departments.user_id', '=', 'users.id')
+                                    ->select('departments.id', 'departments.tower', 'departments.square_meters', 'departments.status',
+                                    'users.name', 'users.phone_number',
+                                    )
+                                    ->where([
+                                        ['departments.department_id', 'like', '%' . $request->department_id . '%'],
+                                        ['users.phone_number', $request->keyword],
+                                        ])
+                                    ->orWhere([
+                                        ['departments.department_id', 'like', '%' . $request->department_id . '%'],
+                                        ['users.name', 'like', '%' . $request->keyword . '%'],
+                                        ])
+                                    ->get();
         }
-        $result = DepartmentResource::collection($departments);
-        return $this->success($result);
+        $departments->load('bills', 'users');
+        return $this->success($departments);
     }
 }
--- resources/views/registerForm.blade.php
+++ resources/views/registerForm.blade.php
@@ -1,18 +1,18 @@
 <form action="" method="POST">
     @csrf
     <div>
-        <label for="">Department ID</label>
+        <label for="">Tên phòng</label>
         <input type="text" name="department_id" id="">
     </div>
     <div>
-        <label for="">Pass</label>
+        <label for="">Mật khẩu</label>
         <input type="password" name="password" id="">
     </div>
     <div>
-        <label for="">Nhập lại</label>
+        <label for="">Nhập lại mật khẩu</label>
         <input type="password" name="password_confirmation" id="">
     </div>
     <div>
-        <button type="submit">ok</button>
+        <button type="submit">Đăng ký</button>
     </div>
 </form>
