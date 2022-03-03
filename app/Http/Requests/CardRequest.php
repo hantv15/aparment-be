7d20eea377daccec1c5ce83e4e42354a0ce4cff6
=ducanh6629417 <=ducanh01273635868@gmail.com>
2022-03-03 16:03:36 +0700

    validate_card_table

---
+++ app/Http/Requests/CardRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CardRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $requestRule = [
+            'number'=>[
+                'required','string',
+                Rule::unique('cards')->ignore($this->id)
+            ],
+            'status'=>'required|integer|min:0|max:1',
+            // 'expire_time'=>'required',
+            'apartment_id'=>'required',
+        ];
+        
+        return $requestRule;
+    }
+}
