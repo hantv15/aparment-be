0edd5d640106d84d607ea3b0d27f13c3c7a0b54e
unknown <huyvdph12786@fpt.edu.vn>
2022-03-26 15:53:46 +0700

    Update index.blade.php

--- resources/views/services/index.blade.php
+++ resources/views/services/index.blade.php
@@ -66,7 +66,7 @@
                             <div class="table-actions d-flex align-items-center gap-3 fs-6">
                                 <a href="javascript:;" class="text-primary" data-bs-toggle="tooltip"
                                     data-bs-placement="bottom" title="Views"><i class="bi bi-eye-fill"></i></a>
-                                <a href="javascript:;" class="text-warning" data-bs-toggle="tooltip"
+                                <a href="{{route('service.edit', ['id' => $service->id])}}" class="text-warning" data-bs-toggle="tooltip"
                                     data-bs-placement="bottom" title="Edit"><i class="bi bi-pencil-fill"></i></a>
                                 <a href="javascript:;" class="text-danger" data-bs-toggle="tooltip"
                                     data-bs-placement="bottom" title="Delete"><i class="bi bi-trash-fill"></i></a>
