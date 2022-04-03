0412bbcdbfa4bd6f73b801e243a0a434534554f8
unknown <huyvdph12786@fpt.edu.vn>
2022-04-03 07:56:35 +0700

    layout client

--- resources/views/layouts/clients/footer.blade.php
+++ resources/views/layouts/clients/footer.blade.php
@@ -1,21 +1,21 @@
-<div class="footer">
+{{-- <div class="footer">
     <p>Copyright © 2022 Edu Meeting Co., Ltd. All Rights Reserved. 
         <br>
         Design: <a href="https://templatemo.com" target="_parent" title="free css templates">TemplateMo</a>
         <br>
         Distibuted By: <a href="https://themewagon.com" target="_blank" title="Build Better UI, Faster">ThemeWagon</a>
       </p>
-  </div>
+  </div> --}}
 
-
-
-    <script src="{{assets('assets/js/isotope.min.js')}}"></script>
-    <script src="{{assets('assets/js/owl-carousel.js')}}"></script>
-    <script src="{{assets('assets/js/lightbox.js')}}"></script>
-    <script src="{{assets('assets/js/tabs.js')}}"></script>
-    <script src="{{assets('assets/js/video.js')}}"></script>
-    <script src="{{assets('assets/js/slick-slider.js')}}"></script>
-    <script src="{{assets('assets/js/custom.js')}}"></script>
+    <script src="{{asset('assets/plugins/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('assets/js/clients/isotope.min.js')}}"></script>
+    <script src="{{asset('assets/js/clients/owl-carousel.js')}}"></script>
+    <script src="{{asset('assets/js/clients/lightbox.js')}}"></script>
+    <script src="{{asset('assets/js/clients/tabs.js')}}"></script>
+    <script src="{{asset('assets/js/clients/video.js')}}"></script>
+    <script src="{{asset('assets/js/clients/slick-slider.js')}}"></script>
+    <script src="{{asset('assets/js/clients/custom.js')}}"></script>
 
     <script>
         //according to loftblog tut
