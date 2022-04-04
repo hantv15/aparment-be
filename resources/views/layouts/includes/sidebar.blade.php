5c0476b0c9fdf4415f5b648d3e8dfa40bd90e247
Tài Duy <duyntph12791@fpt.edu.vn>
2022-04-04 09:34:44 +0700

    up

--- resources/views/layouts/includes/sidebar.blade.php
+++ resources/views/layouts/includes/sidebar.blade.php
@@ -62,7 +62,7 @@
                 <div class="menu-title">Tòa nhà</div>
             </a>
             <ul>
-                <li> <a href="form-elements.html"><i class="bi bi-circle"></i>Danh sách tòa nhà</a>
+                <li> <a href="{{route('building')}}"><i class="bi bi-circle"></i>Danh sách căn hộ</a>
                 </li>
             </ul>
         </li>
@@ -73,7 +73,7 @@
                 <div class="menu-title">Căn hộ</div>
             </a>
             <ul>
-{{--                <li> <a href="{{route('apartment')}}"><i class="bi bi-circle"></i>Danh sách căn hộ</a>--}}
+               <li> <a href="{{route('apartment')}}"><i class="bi bi-circle"></i>Danh sách căn hộ</a>
                 </li>
             </ul>
         </li>
